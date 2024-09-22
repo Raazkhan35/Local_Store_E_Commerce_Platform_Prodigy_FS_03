a9b45bd2772e23a1f83ddfd4d374566dad94d1b2
Raazkhan35 <khanraaz1712@gmail.com>
2024-09-22 17:19:11 +0530

    Create cart views.py

---
+++ cart views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .models import CartItem, Product
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('cart_detail')
+
+def cart_detail(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'cart_detail.html', {'cart_items': cart_items, 'total_price': total_price})
